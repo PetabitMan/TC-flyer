479379f97be8e372ed64356796f45c1eba86d704
paul311 <paul@permantier.de>
2021-05-19 14:30:30 +0200

    replaced butto with fullscreen one

--- server.js
+++ server.js
@@ -1,34 +1,19 @@
-const express = require("express");
-const cors = require("cors");
 const fs = require('fs');
+const http = require('http');
 const https = require('https');
+const privateKey = fs.readFileSync('certificate/server.key', 'utf8');
+const certificate = fs.readFileSync('certificate/server.cert', 'utf8');
 
-
+const credentials = { key: privateKey, cert: certificate };
+const express = require('express');
 const app = express();
-
-app.use(cors())
-app.use(express.json({ extended: false }));
-
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html")
-
 })
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-//   })
-// }
-const server = https.createServer({
-  key: fs.readFileSync(`${__dirname}/localhost-key.pem`, 'utf8'),
-  cert: fs.readFileSync(`${__dirname}/localhost.pem`, 'utf8')
-}, app);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
-
-// process.env.port is used for later commit if it doesnt exist it switches to 5000
-const PORT = process.env.PORT || 8080;
-
-server.listen(PORT, () => console.log(`Up and Running on port ${PORT}`));
+httpServer.listen(8080, '192.168.0.229');
+httpsServer.listen(8443, '192.168.0.229');
