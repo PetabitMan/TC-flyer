1c262898adf91373ace3d5343d6c170697330d37
paul311 <paul@permantier.de>
2021-06-21 13:21:10 +0200

    conditional server for dev and production

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 const fs = require('fs');
-const http = require('http');
-const https = require('https');
-const privateKey = fs.readFileSync('certificate/server.key', 'utf8');
-const certificate = fs.readFileSync('certificate/server.cert', 'utf8');
-
-const credentials = { key: privateKey, cert: certificate };
 const express = require('express');
-const app = express();
 
+const app = express();
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html")
 })
 
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
-
 // httpServer.listen(8080, '192.168.0.229');
 // httpsServer.listen(8443, '192.168.0.229');
+if (process.env.NODE_ENV === "production") {
 
+  app.use(express.static("client/build"));
 
-httpServer.listen(8080, '192.168.0.129');
-httpsServer.listen(8443, '192.168.0.129');
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => console.log(`Up and Running on port ${PORT}`));
+} else {
+  const http = require('http');
+  const https = require('https');
+  const privateKey = fs.readFileSync('certificate/server.key', 'utf8');
+  const certificate = fs.readFileSync('certificate/server.cert', 'utf8');
+  const httpServer = http.createServer(app);
+  const httpsServer = https.createServer(credentials, app);
+  const credentials = { key: privateKey, cert: certificate };
+  httpServer.listen(8080, '192.168.0.129');
+  httpsServer.listen(8443, '192.168.0.129');
+
+}
+
